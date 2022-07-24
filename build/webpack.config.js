66e5787bbd9f59d0394a4d4ee6495a8af063e848
koto <2650006147@qq.com>
2022-07-24 15:21:16 +0800

    webpack vue2重构

---
+++ build/webpack.config.js
@@ -0,0 +1,110 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, '../src/index.js'),
+    login: path.resolve(__dirname, '../src/login.js')
+  },
+  output: {
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, '../dist')
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    compress: true,
+    port: 9000,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024
+          }
+        },
+        generator: {
+          filename: 'images/[name].[hash:6][ext]'
+        }
+      },
+      {
+        test: /\.ejs$/,
+        use: [
+          {
+            loader: 'ejs-loader',
+            options: {
+              esModule: false
+            },
+          },
+        ]
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new UglifyJsPlugin({ sourceMap: false }),
+      new CssMinimizerPlugin()
+    ],
+    splitChunks: {
+      minSize: 30 * 1024,
+      chunks: 'all',
+      name: 'common',
+      cacheGroups: {
+        jQuery: {
+          name: 'jquery',
+          test: /jquery\.js/,
+          chunks: 'all'
+        },
+        'lodash-es': {
+          name: 'lodash-es',
+          test: /lodash-es/,
+          chunks: 'all'
+        }
+      }
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      chunks: ['bundle']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'login.html',
+      template: './src/login.html',
+      chunks: ['login']
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{
+        from: path.resolve(__dirname, '../src/img'),
+        to: path.resolve(__dirname, '../dist/img')
+      }]
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+      chunkFilename: 'css/[name].chunk.css'
+    }),
+    new CleanWebpackPlugin()
+  ]
+}
---
+++ build/webpack.vue.config.js
@@ -0,0 +1,110 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, '../src/main.js')
+  },
+  output: {
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, '../dist')
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist')
+    },
+    compress: true,
+    port: 9000,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024
+          }
+        },
+        generator: {
+          filename: 'images/[name].[hash:6][ext]'
+        }
+      },
+      {
+        test: /\.ejs$/,
+        use: [
+          {
+            loader: 'ejs-loader',
+            options: {
+              esModule: false
+            },
+          },
+        ]
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new UglifyJsPlugin({ sourceMap: false }),
+      new CssMinimizerPlugin()
+    ],
+    splitChunks: {
+      minSize: 30 * 1024,
+      chunks: 'all',
+      name: 'common',
+      cacheGroups: {
+        jQuery: {
+          name: 'jquery',
+          test: /jquery\.js/,
+          chunks: 'all'
+        },
+        'lodash-es': {
+          name: 'lodash-es',
+          test: /lodash-es/,
+          chunks: 'all'
+        }
+      }
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, '../public/index.html'),
+      chunks: ['index']
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{
+        from: path.resolve(__dirname, '../src/img'),
+        to: path.resolve(__dirname, '../dist/img')
+      }]
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+      chunkFilename: 'css/[name].chunk.css'
+    }),
+    new CleanWebpackPlugin(),
+    new VueLoaderPlugin()
+  ]
+}
